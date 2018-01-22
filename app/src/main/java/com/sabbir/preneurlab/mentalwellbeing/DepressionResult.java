f9ed97440afc7d4eb05eee2ca19c52b5af60ea13
SabbirPreneurLab <sabbir.preneurlab@gmail.com>
2018-01-22 13:25:46 +0600

    Third implementation

--- app/src/main/java/com/sabbir/preneurlab/mentalwellbeing/DepressionResult.java
+++ app/src/main/java/com/sabbir/preneurlab/mentalwellbeing/DepressionResult.java
@@ -2,6 +2,7 @@ package com.sabbir.preneurlab.mentalwellbeing;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -15,6 +16,8 @@ import android.widget.TextView;
 public class DepressionResult extends AppCompatActivity{
 
     TextView textView;
+    double progressStatus = 0;
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,25 +37,58 @@ public class DepressionResult extends AppCompatActivity{
         TextView textView2 = findViewById(R.id.textView_2);
         TextView textView4 = findViewById(R.id.textView_4);
         TextView textView_score = findViewById(R.id.textView_score);
-        ProgressBar progressBar = findViewById(R.id.progressBar);
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
 
         toolbarText.setTypeface(raleway);
         toolbarText.setText("  Mental Health");
 
 
         String s = getIntent().getStringExtra("sum_value");
+        final int ii = Integer.parseInt(getIntent().getStringExtra("sum_value"));
 
         textView = findViewById(R.id.textView);
         textView.setText("Your Mental Wellbeing Score");
         textView.setTypeface(raleway);
+
+        textView_score.setTypeface(raleway, Typeface.BOLD);
         textView_score.setText(s);
-        textView_score.setTypeface(raleway);
+        //textView_score.setTypeface(raleway);
 
 
 
         textView2.setText("0");
         int ss = progressBar.getMax();
         textView4.setText(Integer.toString(ss));
+
+
+
+        // Start long running operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                //while (progressStatus < 100) {
+                //while (progressStatus < progressBar.getMax()) {
+                while (progressStatus < ii) {
+                    progressStatus += 1;
+                    // Update the progress bar and display the
+                    //current value in the text view
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress((int) progressStatus);
+                            //textView.setText(progressStatus+"/"+progressBar.getMax());
+                            //textView.setText(progressStatus+"/"+progressBar.getMax());
+                        }
+                    });
+                    try {
+                        // Sleep for 200 milliseconds.
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+
 
     }
 }
